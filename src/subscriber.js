171e199fabe44258e58808a917a1e2a4ce60c65a
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2019-12-02 17:19:29 +0100

    handle errors

--- src/subscriber.js
+++ src/subscriber.js
@@ -4,8 +4,12 @@ import { subscription, projectId } from "./config";
 export default async () => {
   const pubsub = new PubSub({ projectId });
   pubsub.subscription(subscription).on("message", message => {
-    console.log(JSON.parse(message.data.toString("utf8")));
-    message.ack();
+    try {
+      console.log(JSON.parse(message.data.toString("utf8")));
+      message.ack();
+    } catch (e) {
+      console.error("cannot parse message: " + message.data);
+    }
   });
   console.log("Subscriber started");
 };
